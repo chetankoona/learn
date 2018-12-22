719e08707432889ebf6f2ce2e3ea676bb259ec89
chetankoona <chetankoona8@gmail.com>
2018-12-22 17:16:23 +0530

    merge sort completed

--- learningalgorithms/src/learningalgorithms/algo/mergesort/App.java
+++ learningalgorithms/src/learningalgorithms/algo/mergesort/App.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class App {
 
     public static void main(String[] args) {
-        int[] inputArray = {20, 47, 2, 1, 4, 30, 50, 31, 40};
+        int[] inputArray = {9, 7, 3, 1, 6, 3, 2, 6, 8, 9, 2, 3, 0};
         MergeSort sorter = new MergeSort();
         sorter.sort(inputArray);
         System.out.println("Sorted array using mergesort = " + Arrays.toString(inputArray));
--- learningalgorithms/src/learningalgorithms/algo/mergesort/MergeSort.java
+++ learningalgorithms/src/learningalgorithms/algo/mergesort/MergeSort.java
@@ -3,5 +3,51 @@ package learningalgorithms.algo.mergesort;
 public class MergeSort {
 
     public void sort(int[] inputArray) {
+        sort(inputArray, 0, inputArray.length - 1);
+    }
+
+    private void sort(int[] inputArray, int start, int end) {
+        if (end <= start) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        sort(inputArray, start, mid);
+        sort(inputArray, mid + 1, end);
+        merge(inputArray, start, mid, end);
+    }
+
+    private void merge(int[] inputArray, int start, int mid, int end) {
+        int leftSlotIndex = start;
+        int rightSlotIndex = mid + 1;
+        int[] tempArray = new int[end - start + 1];
+        int tempIndex = 0;
+        while (leftSlotIndex <= mid && rightSlotIndex <= end) {
+            if (inputArray[leftSlotIndex] < inputArray[rightSlotIndex]) {
+                tempArray[tempIndex] = inputArray[leftSlotIndex];
+                leftSlotIndex = leftSlotIndex + 1;
+            } else {
+                tempArray[tempIndex] = inputArray[rightSlotIndex];
+                rightSlotIndex = rightSlotIndex + 1;
+            }
+            tempIndex = tempIndex + 1;
+        }
+
+        if (leftSlotIndex <= mid) {
+            while (leftSlotIndex <= mid) {
+                tempArray[tempIndex] = inputArray[leftSlotIndex];
+                leftSlotIndex = leftSlotIndex + 1;
+                tempIndex = tempIndex + 1;
+            }
+        } else if (rightSlotIndex <= end) {
+            while (rightSlotIndex <= end) {
+                tempArray[tempIndex] = inputArray[rightSlotIndex];
+                rightSlotIndex = rightSlotIndex + 1;
+                tempIndex = tempIndex + 1;
+            }
+        }
+
+        for (int i = 0; i < tempArray.length; i++) {
+            inputArray[start + i] = tempArray[i];
+        }
     }
 }
